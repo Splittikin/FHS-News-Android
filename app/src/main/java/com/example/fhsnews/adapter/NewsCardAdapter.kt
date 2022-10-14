247abf49779faf6f33bc427643d628c92ff3ae93
splittikin <splittikin@gmail.com>
2022-10-14 09:47:05 -0400

    add more stuff

---
+++ app/src/main/java/com/example/fhsnews/adapter/NewsCardAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.fhsnews.adapter
+
+// Adapter to find the correct card type to use for an article and inflate it
+
+class NewsCardAdapter {
+}
---
+++ app/src/main/java/com/example/fhsnews/model/Article.kt
@@ -0,0 +1,6 @@
+package com.example.fhsnews.model
+
+// A piece of news, such as an announcement or an upcoming activity
+
+class Article {
+}
