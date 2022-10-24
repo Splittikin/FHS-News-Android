e2ef199841a0fd4cbae01ce71cc700e741b73278
splittikin <splittikin@gmail.com>
2022-10-24 13:45:46 -0400

    TODOs and commenting

--- app/src/main/java/com/example/fhsnews/model/Article.kt
+++ app/src/main/java/com/example/fhsnews/model/Article.kt
@@ -8,16 +8,16 @@ import java.sql.Date
 
 data class Article(
     val isWeather: Boolean, // If true, this card will be the weather card and all other arguments will be ignored TODO: Change to generic type (0 for regular article, 1 for weather, 2 for red/silver, etc.)
-    @DrawableRes val articleThumbnail: Int, // Image for the article, will be scaled and cropped
-    val postedTime: Date, // UNIX time of when the article was posted
-    val timeUntil: Int = 0, // If applicable, UNIX time of when the associated activity will happen (If specified, the timestamp will show how long until that activity happens i.e. "In 4 days" instead of "6 hours ago")
-    val topperText: String, // Activity the card is associated with, such as "Fishers Football" or "FHS Percussion"
-    @DrawableRes val topperIcon: Int, // Emblem for the topper activity
-    val author: String, // Who wrote the article (Will be skipped if blank)
+    @DrawableRes val articleThumbnail: Int, // Image for the article, will be scaled and cropped TODO: Use placeholder if blank
+    val postedTime: Date, // UNIX time of when the article was posted (Showed like "Posted 6 hours ago". Also determines where on the timeline this article appears, so this should be defined even if the time until is intended to be displayed instead) TODO: Formatting
+    val timeUntil: Int = 0, // If applicable, UNIX time of when the associated activity will happen (Skipped if blank. If specified, the timestamp will show how long until that activity happens i.e. "In 4 days" instead of "6 hours ago") TODO: Implementation
+    val topperText: String, // Activity the card is associated with, such as "Fishers Football" or "FHS Percussion" (Skipped if blank)
+    @DrawableRes val topperIcon: Int, // Emblem for the topper activity (Skipped if topper text is blank) TODO: Skip if blank
+    val author: String, // Who wrote the article (Skipped if blank)
     val headline: String, // Headline for the article
     val subtitle: String, // Subtitle displayed below the headline
-    val articlePreview: String,
-    val text: String // Contents of the article TODO: Change to a different text type that can embed images, videos, urls
+    val articlePreview: String, // Article contents, truncated to a certain length. Automatically set if blank TODO: Automatic truncation implementation
+    val text: String // Contents of the article TODO: Change to a different text type that can embed images
 )
 
 // Arguments may be freely added (or removed if we don't end up using them)
