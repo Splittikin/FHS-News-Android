59d51d22ece61a5bb3493ff073b341b50c37ec51
Acoop06 <coopeand001@hsestudents.org>
2022-10-25 09:12:42 -0400

    Tagged articles

--- app/src/main/java/com/example/fhsnews/data/DataSource.kt
+++ app/src/main/java/com/example/fhsnews/data/DataSource.kt
@@ -19,7 +19,7 @@ object DataSource {
             "Weather",
             0,
             "",
-            listOf(),
+            listOf("Weather"),
             "",
             "",
             ""
@@ -32,7 +32,7 @@ object DataSource {
             "Red/Silver Indicator",
             0,
             "",
-            listOf(),
+            listOf("Indicator"),
             "",
             "",
             ""
@@ -45,7 +45,7 @@ object DataSource {
             "Doing Your Mom Class",
             R.drawable.sports_ico,
             "Goobert Scoobert",
-            listOf(),
+            listOf("Sports"),
             "I three stars your Mother",
             "Idote",
             "He He He Haw"
@@ -58,7 +58,7 @@ object DataSource {
             "Doing Your Mom Class",
             R.drawable.club_ico,
             "Goobert Scoobert II",
-            listOf(),
+            listOf("Clubs"),
             "I three stars your Mother (Again)",
             "Again!",
             "I’ve come to make an announcement: Shadow the Hedgehog’s a bitch ass mother fucker. He pissed on my fucking wife. That’s right, he took his hedgehog fuckin' quilly dick out and he pissed on my fucking wife, and he said his dick was THIS BIG. And I said “that’s disgusting!” So I’m making a callout post on my twitter dot com: \"Shadow the Hedgehog, you got a small dick, it’s the size of this walnut except WAY smaller.\" And guess what, here’s what my dong looks like: PFFFFFFFFGJT. That’s right baby. All point, no quills, no pillows, look at that it looks like two balls and a bong. He fucked my wife so guess what, I’m gonna FUCK THE EARTH. THATS RIGHT THIS IS WHAT YOU GET, MY SUPER LAZER PISS. Except I’m not gonna piss on the earth, I’m gonna go higher. I’m pissing on the MOOOOOON! How do you like that, OBAMA? I PISSED ON THE MOON, YOU IDIOT! You have twenty-three hours before the piss d r o p l e t s hit the fucking earth, now get out of my fucking sight before I piss on you too!\n"
