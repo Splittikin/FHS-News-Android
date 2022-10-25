b1a8dd659f2ec3a2889bd773e65b120525523657
splittikin <splittikin@gmail.com>
2022-10-25 09:35:03 -0400

    unshit the fuck

--- app/src/main/java/com/example/fhsnews/MainActivity.kt
+++ app/src/main/java/com/example/fhsnews/MainActivity.kt
@@ -9,12 +9,10 @@ import com.example.fhsnews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
     
     
