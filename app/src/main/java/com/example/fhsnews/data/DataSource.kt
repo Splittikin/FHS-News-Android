151b68f6f76f56730ae5026a6d8f99098765f226
Acoop06 <coopeand001@hsestudents.org>
2022-10-31 13:55:02 -0400

    changed articles

--- app/src/main/java/com/example/fhsnews/data/DataSource.kt
+++ app/src/main/java/com/example/fhsnews/data/DataSource.kt
@@ -30,9 +30,9 @@ object DataSource {
             R.drawable.sports_ico,
             "Goobert Scoobert",
             listOf(),
-            "Lorel Ispum",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            "Fishers wins against HSE!",
+            "The fishers tigers beat the HSE royals 13-7 in the latest game.",
+            "The High school’s rule book outlaws celebrations that are “excessive,” which is a subjective measure. To truly gauge whether a celebration is excessive, you have to determine an appropriate amount of celebration for a certain accomplishment. For a first down when trailing by seven in the first quarter? Anything more than a smile is excessive. But let’s say a player hypothetically made a spectacular Hail Mary touchdown catch at the very back of the end zone—the deepest catch ever recorded—to win a game, putting his team in first place in their division, just weeks after they’d fired their coach. It’s hard to imagine any celebration that would be excessive. Any amount of screaming, gesturing, alcohol consumption, pyrotechnic displays, musical performances, and a certain amount of tasteful stripping would be tolerated. Unfortunately, this was not a hypothetical scenario. In Sunday’s Panthers-Falcons game, with possession of first place in the NFC South on the line and the Panthers trailing by six points, Carolina QB P.J. Walker threw a stunning 62-yard touchdown pass to D.J. Moore."
         ), Article(
             0,
             R.drawable.img_ocean,
@@ -42,9 +42,9 @@ object DataSource {
             R.drawable.club_ico,
             "Goobert Scoobert II",
             listOf(),
-            "Lorel Ispum",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            "Lorel ispum dolor sit amet\n"
+            "Chess club meetings are on wednesdays!",
+            "The chess club meetings are on wednesdays, we hope to see you there!",
+            "The chess club allows any person of any skill level and grade. we are a welcoming community, willing to teach anyone!"
         ), Article(
             0,
             R.drawable.img_ocean,
