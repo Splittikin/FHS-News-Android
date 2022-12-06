ec1f276733462e71166cfdb6daa4c2f1667d23fd
Acoop06 <coopeand001@hsestudents.org>
2022-12-06 09:35:32 -0500

    commit message

--- app/src/main/java/com/example/fhsnews/data/DataSource.kt
+++ app/src/main/java/com/example/fhsnews/data/DataSource.kt
@@ -30,16 +30,18 @@ object DataSource {
             0, 1, R.drawable.gift_wrappinh, Date(1671469200000), 0, "Fundraiser", R.drawable.events_ico, "", listOf(), "Holiday Gift Wrapping", "For the Leukemia and Lymphoma Society", "On December 19th at door 19, remember to drop off 5-10 gifts to be wrapped for a donation via cash, check, or CashApp. You may bring your own wrapping paper if it is specific to your kid. All proceeds go to the Leukemia and Lymphoma Society"
         ), Article(
             0, 2, R.drawable.winter_band_digital_program, Date(1670542200000), 0, "Band", R.drawable.music_ico, "", listOf(), "Sounds of the Season Concert", "Intermediate Jazz Ensemble, Advanced Jazz Ensemble, Concert Band, Symphonic Band Red, Symphonic Band Gold, Wind Symphony, Wind Ensemble", "Enjoy the Sounds of the Season as the FHS Bands play selections to get you into the holiday spirit. The Intermediate Jazz Ensemble will play in the Auditorium Lobby beginning at 6:30 p.m. as we begin seating. The concert will begin in the Auditorium at 7 p.m. and the Concert, Symphonic Band Red, Symphonic Band Gold, Wind Symphony, Wind Ensemble and Advanced Jazz Ensemble bands will perform."
-        )
+        ), /*Article(
+            0,3, R.drawable.orchestra_holiday_digital_monitor, Date(1670371200000), 0, "Orchestra", R.drawable.music_ico, "", listOf(), "Orchestra Holiday Concert"
+        )*/
     )
     val clubsList: List<Club> = listOf(
         Club(
             R.drawable.img_ocean,
-            Date(1665757481000),
+            Date(0),
             listOf(),
-            "The piano club",
-            "We play piano",
-            "Come to our call-out meeting! Aux gym on the 31st!"
+            "Computer Science Club",
+            "Sponsored by Mr Guler, Room C209",
+            "Computer Science Club is a forum for students interested in computer programming, hardware, software, or other CS topics to connect with students who have similar interests and learn from each other. "
         )
     )
 }
