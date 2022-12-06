6d79f7528600280c4d9d90cd2de575de7f662ad8
The Jam <piercove2005@gmail.com>
2022-12-06 08:50:16 -0500

    post this cat when they least expect it

--- app/src/main/java/com/example/fhsnews/data/DataSource.kt
+++ app/src/main/java/com/example/fhsnews/data/DataSource.kt
@@ -27,62 +27,9 @@ object DataSource {
     )
     val newsList: List<Article> = listOf(
         Article(
-            //first article
-            0,
-            0,
-            //picture displayed
-            R.drawable.img_ocean,
-            Date(1665757481000),
-            //for announcements, not implemented yet
-            0,
-            //topper, topic of club
-            "Fishers Boy's Varsity Football",
-            //icon
-            R.drawable.sports_ico,
-            "Goobert Scoobert",
-            listOf(),
-            "Fishers wins against HSE!",
-            "The fishers tigers beat the HSE royals 13-7 in the latest game.",
-            "The High school’s rule book outlaws celebrations that are “excessive,” which is a subjective measure. To truly gauge whether a celebration is excessive, you have to determine an appropriate amount of celebration for a certain accomplishment. For a first down when trailing by seven in the first quarter? Anything more than a smile is excessive. But let’s say a player hypothetically made a spectacular Hail Mary touchdown catch at the very back of the end zone—the deepest catch ever recorded—to win a game, putting his team in first place in their division, just weeks after they’d fired their coach. It’s hard to imagine any celebration that would be excessive. Any amount of screaming, gesturing, alcohol consumption, pyrotechnic displays, musical performances, and a certain amount of tasteful stripping would be tolerated. Unfortunately, this was not a hypothetical scenario. In Sunday’s Panthers-Falcons game, with possession of first place in the NFC South on the line and the Panthers trailing by six points, Carolina QB P.J. Walker threw a stunning 62-yard touchdown pass to D.J. Moore."
+            0, 1, R.drawable.gift_wrappinh, Date(42069), 0, "Weather", 0, "", listOf(), "", "", ""
         ), Article(
-            0,
-            1,
-            R.drawable.img_ocean,
-            Date(1665757481000),
-            0,
-            "Chess Club",
-            R.drawable.club_ico,
-            "Goobert Scoobert II",
-            listOf(),
-            "Chess club meetings are on wednesdays!",
-            "The chess club meetings are on wednesdays, we hope to see you there!",
-            "The chess club allows any person of any skill level and grade. we are a welcoming community, willing to teach anyone!"
-        ), Article(
-            0,
-            2,
-            R.drawable.img_ocean,
-            Date(1665757481000),
-            0,
-            "Computer Science Club",
-            R.drawable.club_ico,
-            "Goobert Scoobert III",
-            listOf(),
-            "Webdev makes new website",
-            "The Webdevs did the thing",
-            "they made a website"
-        ), Article(
-            0,
-            3,
-            R.drawable.img_ocean,
-            Date(1670003304000),
-            0,
-            "Girl's Volleyball",
-            R.drawable.sports_ico,
-            "Goobert Scoobert IV",
-            listOf(),
-            "They win",
-            "Yaaaaaayyyyy",
-            "they are awesome"
+            0, 2, R.drawable., Date(42069), 0, "Red/Silver Indicator", 0, "", listOf(), "", "", ""
         )
     )
     val clubsList: List<Club> = listOf(
