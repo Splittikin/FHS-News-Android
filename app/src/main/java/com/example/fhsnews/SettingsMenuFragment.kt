40edf3bf40a7440cfd5382df7336d54d738648f4
The Jam <piercove2005@gmail.com>
2022-12-06 09:04:53 -0500

    the settings of the settings of the settings of the settings

--- app/src/main/java/com/example/fhsnews/SettingsMenuFragment.kt
+++ app/src/main/java/com/example/fhsnews/SettingsMenuFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import androidx.fragment.app.Fragment
 import com.example.fhsnews.databinding.FragmentSettingsMenuBinding
 
@@ -24,5 +25,27 @@ class SettingsMenuFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.radio_system ->
+                    if (checked) {
+                        // sex
+                    }
+                R.id.radio_light ->
+                    if (checked) {
+                        // sex
+                    }
+                R.id.radio_dark ->
+                    if (checked) {
+                        // sex
+                    }
+            }
+        }
     }
 }
