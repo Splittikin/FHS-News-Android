ffdd63d1dd84cf94c4a9838d34c8fa62c0e95236
Splittikin <45697042+Splittikin@users.noreply.github.com>
2023-01-12 09:23:37 -0500

    Display a (placeholder) loading icon when loading and if it fails, show the error onscreen

--- app/src/main/java/com/example/fhsnews/NewsScrollerBindingAdapters.kt
+++ app/src/main/java/com/example/fhsnews/NewsScrollerBindingAdapters.kt
@@ -2,13 +2,16 @@ package com.example.fhsnews
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.fhsnews.adapter.NewsCardAdapter
 import com.example.fhsnews.model.Article
+import com.example.fhsnews.overview.FHSNewsApiStatus
 
 @BindingAdapter("listData")
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<Article>?) {
@@ -26,5 +29,29 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
             placeholder(R.drawable.ic_baseline_download_24)
             error(R.drawable.ic_baseline_broken_image_24)
         }
+    }
+}
+
+@BindingAdapter("FHSNewsApiStatus")
+fun bindStatus(statusImageView: ImageView, status: FHSNewsApiStatus) {
+    when (status) {
+        FHSNewsApiStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_baseline_download_24)
+        }
+        FHSNewsApiStatus.ERROR -> {
+            statusImageView.visibility = View.GONE
+        }
+        FHSNewsApiStatus.DONE -> {
+            statusImageView.visibility = View.GONE
+        }
+    }
+}
+
+@BindingAdapter("FHSNewsApiStatus")
+fun bindProblem(problemTextView: TextView, problem: String) {
+    if (problem != "") {
+        problemTextView.visibility = View.VISIBLE
+        problemTextView.text = problem
     }
 }
--- app/src/main/java/com/example/fhsnews/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/fhsnews/overview/OverviewViewModel.kt
@@ -10,34 +10,27 @@ import com.example.fhsnews.model.Article
 import com.example.fhsnews.network.FHSNewsApi
 import kotlinx.coroutines.launch
 
+
 enum class FHSNewsApiStatus { LOADING, ERROR, DONE }
 
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData that stores the status of the most recent request
     private val _status = MutableLiveData<FHSNewsApiStatus>()
 
-    // The external immutable LiveData for the request status
     val status: LiveData<FHSNewsApiStatus> = _status
 
-    // Internally, we use a MutableLiveData, because we will be updating the List of MarsPhoto
-    // with new values
+    private val _problem = MutableLiveData<String>("")
+
+    val problem: LiveData<String> = _problem
+
     private val _articles = MutableLiveData<List<Article>>()
 
-    // The external LiveData interface to the property is immutable, so only this class can modify
     val articles: LiveData<List<Article>> = _articles
 
-    /**
-     * Call getMarsPhotos() on init so we can display status immediately.
-     */
     init {
         getArticles()
     }
 
-    /**
-     * Gets Mars photos information from the Mars API Retrofit service and updates the
-     * [MarsPhoto] [List] [LiveData].
-     */
     private fun getArticles() {
 
         viewModelScope.launch {
@@ -48,6 +41,7 @@ class OverviewViewModel : ViewModel() {
                 _status.value = FHSNewsApiStatus.DONE
             } catch (e: Exception) {
                 _status.value = FHSNewsApiStatus.ERROR
+                _problem.value = e.toString()
                 Log.e(TAG, "getArticles: couldn't get articles: $e")
                 _articles.value = listOf()
             }
